9da7f7749504f8de34871ce4d1e4ef4bbb554b74
Christopher Bautista <cbaut@Christophers-Laptop.local>
2024-05-01 22:25:22 -0700

    edit code for it to gen markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,41 +1,52 @@
+// Function to render license section
 function renderLicenseSection(license) {
   switch (license) {
     case 'MIT':
-      return `This project is licensed under the ${renderLicenseLink(license)}.`;
+      return `This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).`;
     case 'GPL':
-      return `This project is licensed under the ${renderLicenseLink(license)}.`;
+      return `This project is licensed under the [GPL License](https://opensource.org/licenses/GPL-3.0).`;
     case 'Apache':
-      return `This project is licensed under the ${renderLicenseLink(license)}.`;
+      return `This project is licensed under the [Apache License](https://opensource.org/licenses/Apache-2.0).`;
     default:
       return ''; // Return empty string if license is not recognized
   }
 }
 
-// Function to render license badge
-function renderLicenseBadge(license) {
-  switch (license) {
-    case 'MIT':
-      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-    case 'GPL':
-      return `[![License: GPL](https://img.shields.io/badge/License-GPL-blue.svg)](https://opensource.org/licenses/GPL-3.0)`;
-    case 'Apache':
-      return `[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-    default:
-      return ''; // Return empty string if license is not recognized
-  }
-}
+// Function to generate markdown for README
+function generateMarkdown(data) {
+  const licenseSection = renderLicenseSection(data.license);
+  return `
+# ${data.title}
 
-function renderLicenseLink(license) {
-  switch (license) {
-    case 'MIT':
-      return `[MIT License](https://opensource.org/licenses/MIT)`;
-    case 'GPL':
-      return `[GPL License](https://opensource.org/licenses/GPL-3.0)`;
-    case 'Apache':
-      return `[Apache License](https://opensource.org/licenses/Apache-2.0)`;
-    default:
-      return ''; // Return empty string if license is not recognized
-  }
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${licenseSection}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+${data.questions}
+`;
 }
 
 module.exports = generateMarkdown;
