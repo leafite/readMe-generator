d3e5da3bb49e9d9690d5de30dff533188227f3be
Christopher Bautista <cbaut@Christophers-Laptop.local>
2024-05-01 22:00:38 -0700

    change to js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,41 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'MIT':
+      return `This project is licensed under the ${renderLicenseLink(license)}.`;
+    case 'GPL':
+      return `This project is licensed under the ${renderLicenseLink(license)}.`;
+    case 'Apache':
+      return `This project is licensed under the ${renderLicenseLink(license)}.`;
+    default:
+      return ''; // Return empty string if license is not recognized
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to render license badge
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    case 'GPL':
+      return `[![License: GPL](https://img.shields.io/badge/License-GPL-blue.svg)](https://opensource.org/licenses/GPL-3.0)`;
+    case 'Apache':
+      return `[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    default:
+      return ''; // Return empty string if license is not recognized
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return `[MIT License](https://opensource.org/licenses/MIT)`;
+    case 'GPL':
+      return `[GPL License](https://opensource.org/licenses/GPL-3.0)`;
+    case 'Apache':
+      return `[Apache License](https://opensource.org/licenses/Apache-2.0)`;
+    default:
+      return ''; // Return empty string if license is not recognized
+  }
 }
 
 module.exports = generateMarkdown;
