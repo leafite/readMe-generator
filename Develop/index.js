3350e726548ea86b6052196c87f1ff27ade6e77f
Christopher Bautista <cbaut@Christophers-Laptop.local>
2024-05-01 22:24:27 -0700

    added main code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,78 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Destructure the required functions from generateMarkdown.js
+const { renderLicenseSection, renderLicenseBadge, renderLicenseLink } = generateMarkdown;
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Write a brief description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'Specify the license for your project:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Explain how others can contribute to your project:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide instructions for running tests:',
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'Provide contact information for any questions:',
+  },
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  const readMe = generateMarkdown(data);
+  fs.writeFile(fileName, readMe, err => {
+    if (err) {
+      console.error('Error writing README:', err);
+      return;
+    }
+    console.log(`README file "${fileName}" has been created successfully!`);
+  });
+}
+
+// Function to initialize app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then(answers => {
+      writeToFile('README.md', answers);
+    })
+    .catch(error => {
+      console.error('Error occurred during initialization:', error);
+    });
+}
 
 // Function call to initialize app
 init();
